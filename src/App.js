b0410c2b266440c7800d25f3154a7f8a0fe1c133
Abraham <abrahamokaalo@gmail.com>
2023-11-07 09:52:43 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [location, setLocation] = useState("lisbon");
+
+  function handleFetchWeather() {
+    console.log("Loading Data...");
+    console.log(this);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Classy Weather</h1>
+      <input
+        type="text"
+        placeholder="Search from location ..."
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button onClick={handleFetchWeather}>Get Weather</button>
     </div>
   );
 }
-
-export default App;
